be45ad9ab3a02763dac5217443369503e1b468c2
Tattape Sonklin <tsc_com@hotmail.com>
2023-11-13 20:47:11 +0700

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
     output: 'export',
-    reactStrictMode: true,
-    assetPrefix: isProd ? '/restaurant/' : '',
+    // distDir: 'dist',
+    images: {
+        unoptimized: true
+    }
 }
 
 module.exports = nextConfig
