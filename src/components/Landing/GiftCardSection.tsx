10bc5643a8e6297161f90829b925f5351063ff53
WNKing <77584564+BlazingKing@users.noreply.github.com>
2023-11-18 02:12:33 +0700

    style: responsive gift card section

--- src/components/Landing/GiftCardSection.tsx
+++ src/components/Landing/GiftCardSection.tsx
@@ -2,76 +2,84 @@ import React, { useEffect, useState } from "react";
 import { Parallax } from "react-scroll-parallax";
 import Image from "next/image";
 
-
 import giftCardBG from "../../images/giftCardBG.png";
 import giftCard from "../../images/gift-card.png";
 import thongTong from "../../images/thongtong.png";
 import heart from "../../images/heart.png";
 import women from "../../images/women.png";
 
-type Props = {}
+type Props = {};
 
-function GiftCardSection({ }: Props) {
-    return (
-        <div className="h-[750px]">
-            <div className="relative">
-                <Parallax speed={5} translateX={[0, -10]}>
-                    <div className="w-full 2xl:left-[70px] xl:-left-[10px] z-0">
-                        <Image
-                            width={2200}
-                            className="w-full"
-                            height={900}
-                            src={giftCardBG}
-                            alt={""}
-                        />
-                    </div>
-                </Parallax>
-                <Parallax translateX={[50, -250]}>
-                    <div className="absolute rounded-full bg-[#9f00a3] opacity-70 2xl:top-[-650px] 2xl:right-[450px] xl:top-[-480px] xl:right-[515px] border-none 2xl:w-[480px] 2xl:h-[480px] xl:w-[350px] xl:h-[350px]"></div>
-                </Parallax>
-                <Parallax translateX={[-50, 238]}>
-                    <div className="absolute 2xl:top-[-610px] 2xl:right-[0px] 2xl:left-[570px] xl:top-[-450px] xl:right-[0px] xl:left-[599px] 2xl:w-[500px] 2xl:h-[200px] xl:w-[350px] xl:h-[200px]">
-                        <Image width={600} height={100} src={giftCard} alt={""} />
-                    </div>
-                </Parallax>
-                <Parallax speed={-10}>
-                    <div className="absolute h-[260px] w-[260px] 2xl:left-[1450px] 2xl:-top-[520px] xl:left-[1060px] xl:-top-[425px]">
-                        <Image
-                            width={500}
-                            height={800}
-                            src={women}
-                            alt={""}
-                            unoptimized
-                        />
-                    </div>
-                </Parallax>
-                <Parallax translateY={[-20, 0]}>
-                    <div className="absolute 2xl:w-[200px] 2xl:h-[200px] xl:w-[150px] xl:h-[150px] 2xl:left-[1330px] 2xl:top-[-405px] xl:left-[985px] xl:top-[-320px]">
-                        <Image width={500} height={30} src={heart} alt={""} />
-                    </div>
-                </Parallax>
-                <Parallax translateX={[-85, 420]}>
-                    <div className="absolute 2xl:w-[280px] 2xl:h-[280px] xl:w-[200px] xl:h-[200px] 2xl:left-[1060px] 2xl:top-[-530px] xl:left-[1050px] xl:top-[-400px]">
-                        <Image width={500} height={30} src={thongTong} alt={""} />
-                    </div>
-                </Parallax>
-                <Parallax translateX={[0, 10]}>
-                    <div className="absolute 2xl:left-[650px] 2xl:top-[-280px] xl:left-[500px] xl:top-[-225px]">
-                        <div className="text-white 2xl:text-3xl xl:text-xl">
-                            Give your loved ones the gift of dining
-                        </div>
-                    </div>
-                </Parallax>
-                <Parallax translateX={[10, 0]}>
-                    <div className="absolute 2xl:left-[730px] 2xl:top-[-245px] xl:left-[530px] xl:top-[-199px]">
-                        <p className="text-white 2xl:text-3xl xl:text-xl">
-                            with Bai Tong gift card.
-                        </p>
-                    </div>
-                </Parallax>
+function GiftCardSection({}: Props) {
+  return (
+    <div className="my-[200px]">
+      <div className="flex flex-col items-center">
+        {/* Thong Muang */}
+        <Parallax>
+          <div className="2xl:w-full xl:w-full lg:w-full md:w-full l:w-[700px] m:w-[600px] w-[500px] 2xl:left-[70px] xl:-left-[10px]">
+            <Image
+              width={2200}
+              className="w-full"
+              height={900}
+              src={giftCardBG}
+              alt={""}
+            />
+          </div>
+        </Parallax>
+
+        {/* All Images */}
+        <div className="flex 2xl:mt-[-630px] xl:mt-[-480px] lg:mt-[-350px] lg:mr-[200px] md:mt-[-250px] md:mr-[300px] l:mt-[-270px] l:mr-[290px] m:mt-[-230px] m:mr-[280px] mt-[-195px] mr-[260px]">
+          <div className="2xl:mt-[60px] 2xl:mr-[-210px] xl:mt-[30px] xl:mr-[-160px] lg:mt-[25px] lg:mr-[-150px] md:mt-[8px] md:mr-[-120px] l:mr-[-160px] m:mr-[-200px] mr-[-200px] flex flex-col 2xl:gap-20 xl:gap-16 lg:gap-14 md:gap-9 l:gap-32 m:gap-28 gap-20 z-10">
+            {/* Gift Card */}
+            <Parallax>
+              <div className="2xl:ml-[20px] xl:ml-[100px] lg:ml-[190px] md:ml-[250px] l:ml-[295px] m:ml-[280px] ml-[270px] 2xl:w-[400px] xl:w-[350px] lg:w-[250px] md:w-[200px] l:w-[150px] m:w-[100px] w-[100px]">
+                <Image width={600} height={100} src={giftCard} alt={""} />
+              </div>
+            </Parallax>
+            {/* Description */}
+            <div className="flex flex-col w-[520px] items-end">
+              <Parallax>
+                <p className="text-white 2xl:text-2xl xl:text-xl lg:text-base md:text-sm l:text-sm m:text-sm text-xs">
+                  Give your loved ones the gift of dining
+                </p>
+              </Parallax>
+              <Parallax>
+                <p className="text-white 2xl:text-2xl xl:text-xl lg:text-base md:text-sm l:text-sm m:text-sm text-xs">
+                  with Bai Tong gift card.
+                </p>
+              </Parallax>
             </div>
+          </div>
+
+          <div className="flex">
+            {/* Cicle */}
+            <Parallax>
+              <div className="md:mt-0 l:mt-[45px] m:mt-[20px] mt-[20px] rounded-full bg-[#9f00a3] border-none opacity-70 2xl:w-[480px] 2xl:h-[480px] xl:w-[350px] xl:h-[350px] lg:w-[280px] lg:h-[280px] md:h-[200px] md:w-[200px] l:w-[180px] l:h-[180px] m:w-[160px] m:h-[160px] w-[140px] h-[140px]"></div>
+            </Parallax>
+            {/* thongtong */}
+            <Parallax className="z-20">
+              <div className="2xl:ml-[-340px] 2xl:mt-[65px] xl:ml-[-250px] xl:mt-[65px] lg:ml-[-200px] lg:mt-[40px] md:ml-[-150px] md:mt-[20px] l:ml-[-140px] l:mt-[80px] m:ml-[-120px] m:mt-[55px] ml-[-100px] mt-[55px] 2xl:w-[280px] xl:w-[200px] lg:w-[160px] md:w-[120px] l:w-[120px] m:w-[100px] w-[80px]">
+                <Image width={500} height={30} src={thongTong} alt={""} />
+              </div>
+            </Parallax>
+            {/* Heart */}
+            <Parallax className="z-10">
+              <div className="2xl:mt-[180px] 2xl:ml-[-120px] xl:mt-[120px] xl:ml-[-100px] lg:mt-[100px] lg:ml-[-80px] md:mt-[70px] md:ml-[-60px] l:mt-[110px] l:ml-[-40px] m:mt-[75px] m:ml-[-40px] ml-[-40px] mt-[60px] 2xl:w-[200px] xl:w-[180px] lg:w-[120px] md:w-[80px] l:w-[60px] m:w-[60px] w-[60px]">
+                <Image width={500} height={30} src={heart} alt={""} />
+              </div>
+            </Parallax>
+          </div>
+
+          {/* Woman */}
+          <Parallax>
+            <div className="2xl:ml-[-80px] 2xl:mt-[-50px] xl:ml-[-80px] xl:mt-[-50px] lg:ml-[-35px] lg:mt-[-5px] md:ml-[-20px] md:mt-[-5px] l:ml-[-25px] l:mt-[25px] m:ml-[-25px] m:mt-[10px] mt-[15px] ml-[-20px] 2xl:w-[330px] xl:w-[260px] lg:w-[160px] md:w-[120px] l:w-[120px] m:w-[110px] w-[80px]">
+              <Image width={500} height={800} src={women} alt={""} />
+            </div>
+          </Parallax>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default GiftCardSection
+export default GiftCardSection;
