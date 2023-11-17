b6e180bbade12960a167e82c9d4ef581561d14af
Tattape Sonklin <tsc_com@hotmail.com>
2023-11-18 01:02:05 +0700

    style: layout and animation, responsive

--- src/Utils/SmallPlateImg.js
+++ src/Utils/SmallPlateImg.js
@@ -8,4 +8,14 @@ import plate7 from '../images/SmallPlate/plate7.png'
 import plate8 from '../images/SmallPlate/plate8.png'
 import plate9 from '../images/SmallPlate/plate9.png'
 
+plate1.name = 'Spring Rolls'
+plate2.name = 'Chicken Wings'
+plate3.name = 'Crab Wontons'
+plate4.name = 'Fried Tofu'
+plate5.name = 'Grilled Beef'
+plate6.name = 'Northeastern Thai Fermented Sausage'
+plate7.name = 'Fresh roll'
+plate8.name = 'Tod Mun Pla'
+plate9.name = 'Spare Ribs'
+
 export default [plate1, plate2, plate3, plate4, plate5, plate6, plate7, plate8, plate9]
--- src/components/Landing/SmallPlateSection.tsx
+++ src/components/Landing/SmallPlateSection.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Parallax } from "react-scroll-parallax";
 import Image from "next/image";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SmallPlateImg from '../../Utils/SmallPlateImg';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+
+// import required modules
+import { EffectCoverflow } from 'swiper/modules';
 
 
 import menu from "../../images/menu.png";
 import smallPlates from "../../images/small-plate.png";
-import brife from "../../images/brife.png";
-import chicken from "../../images/chicken.png";
-import sanvid from "../../images/sanvid.png";
 import handblack from "../../images/hand-black.png";
 import bitong from "../../images/bitong.png";
 
@@ -18,101 +22,71 @@ import bitong from "../../images/bitong.png";
 type Props = {}
 
 function SmallPlateSection({ }: Props) {
-    const settings = {
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 3,
-        swipeToSlide: true,
-        arrows: false,
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        centerMode: true,
-        centerPadding: "0",
-    };
+    const [activeIndex, setActiveIndex] = useState(Math.floor(SmallPlateImg.length / 2));
 
-    const smallPlate = [
-        {
-            src: sanvid,
-            alt: "image1",
-        },
-        {
-            src: chicken,
-            alt: "image2",
-        },
-        {
-            src: brife,
-            alt: "image3",
-        },
-        {
-            src: sanvid,
-            alt: "image1",
-        },
-        {
-            src: brife,
-            alt: "image3",
-        },
-        {
-            src: chicken,
-            alt: "image2",
-        },
-        {
-            src: sanvid,
-            alt: "image1",
-        },
-        {
-            src: chicken,
-            alt: "image2",
-        },
-        {
-            src: brife,
-            alt: "image3",
-        },
-        {
-            src: sanvid,
-            alt: "image1",
-        },
-    ];
     return (
-        <div className=" md:pt-[120px] lg:pt-0 my-[120px]">
-            <div className="relative">
+        <div className="my-[300px]">
+            <div className="flex flex-col items-center">
                 <div className="flex justify-center">
                     <div className="flex flex-col gap-6 items-center w-[180px] md:w-[300px] lg:w-[500px]">
                         <Image data-aos="fade-up" src={menu} alt={""} />
                         <Image data-aos="fade-down" src={smallPlates} alt={""} />
                     </div>
                 </div>
-                <Parallax translateX={[10, 0]} translateY={[0, 0]} speed={10}>
-                    <div className="absolute -top-[180px] right-[0px] md:w-[200px] lg:w-[350px] ">
-                        <Image width={600} height={600} src={handblack} alt={""} />
-                    </div>
-                </Parallax>
-                <div data-aos="zoom-in-down" className=" w-full md:top-[300px] lg:top-[400px]">
-                    <Slider
-                        {...settings}
-                        className="flex items-center justify-center"
-                    >
-                        {smallPlate.map((item, idx) => (
-                            <div key={idx} className="outline-none cursor-grab">
-                                <Image
-                                    width={500}
-                                    height={500}
-                                    key={idx}
-                                    src={item.src}
-                                    alt={item.alt}
-                                    className="p-3 hover:scale-[1.05] transition-all object-contain"
-                                />
-                            </div>
-                        ))}
-                    </Slider>
+                <div className="w-full flex justify-end -mt-20 md:-mt-40">
+                    <Parallax translateX={[100, -10]}>
+                        <div className="w-[150px] md:w-[200px] lg:w-[350px]">
+                            <Image width={600} height={600} src={handblack} alt={""} />
+                        </div>
+                    </Parallax>
                 </div>
-                <Parallax speed={-10}>
-                    <div className="absolute -top-[50px] left-[435px] md:w-[250px] lg:w-[300px]">
-                        <Image data-aos="fade-up" data-aos-duration="500" width={300} height={300} src={bitong} alt={""} />
-                    </div>
-                </Parallax>
+                <div className='mt-0 lg:-mt-24' data-aos="zoom-in-down">
+                    <Swiper
+                        effect={'coverflow'}
+                        grabCursor={true}
+                        initialSlide={activeIndex}
+                        centeredSlides={true}
+                        slidesPerView={'auto'}
+                        coverflowEffect={{
+                            rotate: 0,
+                            stretch: 10,
+                            depth: 200,
+                            modifier: 3,
+                            slideShadows: false,
+                        }}
+                        modules={[EffectCoverflow]}
+                        className="!w-full"
+                        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+                    >
+                        {SmallPlateImg.map((image: any, idx: number) => {
+                            const isActive = idx === activeIndex;
+                            const Opacity = isActive ? 'hover:!scale-[1.02] !transition-all' : 'brightness-50';
+
+                            return (
+                                <SwiperSlide key={idx} className={`pb-1 !flex !flex-col !justify-end !w-[300px] !h-[300px] lg:!w-[500px] lg:!h-[500px] ${Opacity}`}>
+                                    <Image width={500} height={500} alt='' src={image.src} />
+                                    {isActive &&
+                                        <p
+                                            data-aos="zoom-in"
+                                            data-aos-anchor-placement="top-bottom"
+                                            className='text-white text-center text-2xl lg:text-3xl'>
+                                            {image.name}
+                                        </p>
+                                    }
+                                </SwiperSlide>
+                            )
+                        })}
+                    </Swiper>
+                </div>
+                <div className="w-full pl-5 md:pl-10 lg:pl-60">
+                    <Parallax speed={-10}>
+                        <div className="w-[150px] md:w-[200px] lg:w-[300px]">
+                            <Image data-aos="fade-down" data-aos-duration="500" width={300} height={300} src={bitong} alt={""} />
+                        </div>
+                    </Parallax>
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
 
