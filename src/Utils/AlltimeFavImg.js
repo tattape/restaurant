1122b51a9409c17c55acc4fada5964cfaf469bed
Tattape Sonklin <tsc_com@hotmail.com>
2023-11-18 18:35:53 +0700

    style: new responsive and layout

---
+++ src/Utils/AlltimeFavImg.js
@@ -0,0 +1,58 @@
+import plate1 from '../images/Alltimes/plate1.png'
+import plate2 from '../images/Alltimes/plate2.png'
+import plate3 from '../images/Alltimes/plate3.png'
+import plate4 from '../images/Alltimes/plate4.png'
+import plate5 from '../images/Alltimes/plate5.png'
+import plate6 from '../images/Alltimes/plate6.png'
+import plate7 from '../images/Alltimes/plate7.png'
+import plate8 from '../images/Alltimes/plate8.png'
+import plate9 from '../images/Alltimes/plate9.png'
+import plate10 from '../images/Alltimes/plate10.png'
+import plate11 from '../images/Alltimes/plate11.png'
+import plate12 from '../images/Alltimes/plate12.png'
+import plate13 from '../images/Alltimes/plate13.png'
+import plate14 from '../images/Alltimes/plate14.png'
+import plate15 from '../images/Alltimes/plate15.png'
+import plate16 from '../images/Alltimes/plate16.png'
+import plate17 from '../images/Alltimes/plate17.png'
+import plate18 from '../images/Alltimes/plate18.png'
+
+plate1.name = 'Gai Hor Bai Toey'
+plate2.name = 'Mas-Sa-Mun Curry'
+plate3.name = 'Red Curry'
+plate4.name = 'Yellow Curry'
+plate5.name = 'Pad Ka Prau'
+plate6.name = 'Crab Fried Rice'
+plate7.name = 'Beef Salad'
+plate8.name = 'Larb'
+plate9.name = 'Crispy Garlic Chicken'
+plate10.name = 'Garlic Delight'
+plate11.name = 'Cashew Nut Chicken'
+plate12.name = 'Swimming Rama'
+plate13.name = 'Mixed Vegetable Deluxe'
+plate14.name = 'Green Curry'
+plate15.name = 'Pa-Nang Curry'
+plate16.name = 'Tom Kah Soup'
+plate17.name = 'Tom Yum Soup'
+plate18.name = 'Yum Woon Sen'
+
+export default [
+    plate1,
+    plate2,
+    plate3,
+    plate4,
+    plate5,
+    plate6,
+    plate7,
+    plate8,
+    plate9,
+    plate10,
+    plate11,
+    plate12,
+    plate13,
+    plate14,
+    plate15,
+    plate16,
+    plate17,
+    plate18,
+]
