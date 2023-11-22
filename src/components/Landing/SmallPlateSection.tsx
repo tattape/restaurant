6cd560d8f0d999c1212044657a95138b9a98c61f
WNKing <77584564+BlazingKing@users.noreply.github.com>
2023-11-22 20:28:36 +0700

    style: revers ui small plate section

--- src/components/Landing/SmallPlateSection.tsx
+++ src/components/Landing/SmallPlateSection.tsx
@@ -16,7 +16,6 @@ import menu from "../../images/menu.png";
 import smallPlates from "../../images/small-plate.png";
 import handblack from "../../images/hand-black.png";
 import bitong from "../../images/bitong.png";
-import { motion, Variants } from "framer-motion";
 
 type Props = {};
 
@@ -24,188 +23,100 @@ function SmallPlateSection({}: Props) {
   const [activeIndex, setActiveIndex] = useState(
     Math.floor(SmallPlateImg.length / 2)
   );
-  const rtolVariants: Variants = {
-    onscreen: {
-      translateX: [100, 30],
-      transition: {
-        bounce: 0.4,
-        duration: 0.8,
-      },
-    },
-  };
-  const ttobVariants: Variants = {
-    offscreen: {
-      opacity: 0,
-    },
-    onscreen: {
-      translateY: 20,
-      opacity: 1,
-      transition: {
-        bounce: 0.4,
-        duration: 0.8,
-      },
-    },
-  };
-  const btotVariants: Variants = {
-    offscreen: {
-      opacity: 0,
-    },
-    onscreen: {
-      translateY: -20,
-      opacity: 1,
-      transition: {
-        bounce: 0.4,
-        duration: 0.8,
-      },
-    },
-  };
-  const textswiperVariants: Variants = {
-    offscreen: {
-      opacity: 0,
-      translateY: 0,
-    },
-    onscreen: {
-      opacity: 1,
-      translateY: -5,
-      transition: {
-        bounce: 0.4,
-        duration: 0.5,
-      },
-    },
-  };
-  const swiperVariants: Variants = {
-    offscreen: {
-    },
-    onscreen: {
-      translateY: 100,
-      transition: {
-        bounce: 0.4,
-        duration: 0.5,
-      },
-    },
-  };
 
   return (
     <div className="mt-0 md:mt-[300px]">
       <div className="flex flex-col items-center">
         <div className="flex justify-center">
-          <div className="flex flex-col gap-1 items-center w-[180px] md:w-[300px] lg:w-[500px]">
-            <motion.div
-              initial="offscreen"
-              whileInView="onscreen"
-              viewport={{ once: true, amount: 0.8 }}
-            >
-              <motion.div
-                className="flex justify-center w-[100px] md:w-[300px] lg:w-[500px]"
-                variants={btotVariants}
-              >
-                <Image src={menu} alt={""} />
-              </motion.div>
-            </motion.div>
-            <motion.div
-              initial="offscreen"
-              whileInView="onscreen"
-              viewport={{ once: true, amount: 0.8 }}
-            >
-              <motion.div
-                className="flex justify-center w-[180px] md:w-[300px] lg:w-[500px]"
-                variants={ttobVariants}
-              >
-                <Image src={smallPlates} alt={""} />
-              </motion.div>
-            </motion.div>
+          <div className="flex flex-col gap-6 items-center w-[180px] md:w-[300px] lg:w-[500px]">
+            <div className="flex justify-center w-[100px] md:w-[300px] lg:w-[500px]">
+              <Image
+                data-aos="fade-up"
+                data-aos-once="true"
+                src={menu}
+                alt={""}
+              />
+            </div>
+            <div className="flex justify-center w-[180px] md:w-[300px] lg:w-[500px]">
+              <Image
+                data-aos="fade-down"
+                data-aos-once="true"
+                src={smallPlates}
+                alt={""}
+              />
+            </div>
           </div>
         </div>
         <div className="w-full flex justify-end -mt-20 md:-mt-40">
-          <motion.div
-            initial="offscreen"
-            whileInView="onscreen"
-            viewport={{ once: true, amount: 0.8 }}
-          >
-            <motion.div
-              className="w-[150px] md:w-[200px] lg:w-[350px]"
-              variants={rtolVariants}
-            >
+          <Parallax translateX={[100, -10]}>
+            <div className="w-[150px] md:w-[200px] lg:w-[350px]">
               <Image width={600} height={600} src={handblack} alt={""} />
-            </motion.div>
-          </motion.div>
+            </div>
+          </Parallax>
         </div>
-        <motion.div
-          initial="offscreen"
-          whileInView="onscreen"
-          viewport={{ once: true, amount: 0.8 }}
+        <div
+          data-aos="zoom-in-down"
+          data-aos-once="true"
           className="mt-0 md:mt-10 lg:-mt-24"
         >
-          <motion.div variants={swiperVariants}>
-            <Swiper
-              effect={"coverflow"}
-              grabCursor={true}
-              initialSlide={activeIndex}
-              centeredSlides={true}
-              slidesPerView={"auto"}
-              coverflowEffect={{
-                rotate: 0,
-                stretch: 10,
-                depth: 200,
-                modifier: 3,
-                slideShadows: false,
-              }}
-              modules={[EffectCoverflow]}
-              className="!w-full"
-              onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
-            >
-              {SmallPlateImg.map((image: any, idx: number) => {
-                const isActive = idx === activeIndex;
-                const Opacity = isActive
-                  ? "hover:!scale-[1.02] !transition-all"
-                  : "brightness-50";
-
-                return (
-                  <SwiperSlide
-                    key={idx}
-                    className={`pb-1 !flex !flex-col !justify-end !w-[200px] !h-[200px] md:!w-[300px] md:!h-[300px] lg:!w-[500px] lg:!h-[500px] ${Opacity}`}
-                  >
-                    <Image width={500} height={500} alt="" src={image.src} />
-                    {isActive && (
-                      <motion.div
-                        initial="offscreen"
-                        whileInView="onscreen"
-                        viewport={{ once: true, amount: 0.8 }}
-                      >
-                        <motion.p
-                          variants={textswiperVariants}
-                          className="text-white text-center text-base md:text-2xl lg:text-3xl font-uid"
-                        >
-                          {image.name}
-                        </motion.p>
-                      </motion.div>
-                    )}
-                  </SwiperSlide>
-                );
-              })}
-            </Swiper>
-          </motion.div>
-        </motion.div>
-        <div className="w-full mt-5 md:mt-0 pl-5 md:pl-10 lg:pl-60">
-          <motion.div
-            initial="offscreen"
-            whileInView="onscreen"
-            viewport={{ once: true, amount: 0.8 }}
+          <Swiper
+            effect={"coverflow"}
+            grabCursor={true}
+            initialSlide={activeIndex}
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 10,
+              depth: 200,
+              modifier: 3,
+              slideShadows: false,
+            }}
+            modules={[EffectCoverflow]}
+            className="!w-full"
+            onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
           >
-            <motion.div
-              className="w-[150px] md:w-[200px] lg:w-[300px]"
-              variants={ttobVariants}
-            >
+            {SmallPlateImg.map((image: any, idx: number) => {
+              const isActive = idx === activeIndex;
+              const Opacity = isActive
+                ? "hover:!scale-[1.02] !transition-all"
+                : "brightness-50";
+
+              return (
+                <SwiperSlide
+                  key={idx}
+                  className={`pb-1 !flex !flex-col !justify-end !w-[200px] !h-[200px] md:!w-[300px] md:!h-[300px] lg:!w-[500px] lg:!h-[500px] ${Opacity}`}
+                >
+                  <Image width={500} height={500} alt="" src={image.src} />
+                  {isActive && (
+                    <p
+                      data-aos="zoom-in"
+                      data-aos-anchor-placement="top-bottom"
+                      data-aos-once="true"
+                      className="text-white text-center text-base md:text-2xl lg:text-3xl font-uid"
+                    >
+                      {image.name}
+                    </p>
+                  )}
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
+        <div className="w-full mt-5 md:mt-0 pl-5 md:pl-10 lg:pl-60">
+          <Parallax speed={-10}>
+            <div className="w-[150px] md:w-[200px] lg:w-[300px]">
               <Image
-                //   data-aos="fade-down"
-                //   data-aos-duration="500"
+                data-aos="fade-down"
+                data-aos-duration="500"
+                data-aos-once="true"
                 width={300}
                 height={300}
                 src={bitong}
                 alt={""}
               />
-            </motion.div>
-          </motion.div>
+            </div>
+          </Parallax>
         </div>
       </div>
     </div>
