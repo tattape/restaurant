09ff6d408f4a8db5dcb436eed363efed21638275
WNKing <77584564+BlazingKing@users.noreply.github.com>
2024-03-01 23:58:59 +0700

    style: resize header gallery

--- src/components/Landing/GallerySection.tsx
+++ src/components/Landing/GallerySection.tsx
@@ -21,11 +21,15 @@ export default function GallerySection() {
       id="gallery"
       className="mt-[150px] h-full w-full flex flex-col gap-16 items-center justify-center"
     >
-      <div data-aos="fade-up" data-aos-once="true" className="lg:w-full md:w-[250px] flex justify-center">
+      <div
+        data-aos="fade-up"
+        data-aos-once="true"
+        className="w-[200px] m:w-[250px] md:w-[300px] lg:w-[400px] flex justify-center"
+      >
         <Image
           className=" hover:drop-shadow-[0_5px_10px_rgba(4,255,158,0.8)] !transition-all !ease-in-out"
-          width={600}
-          height={600}
+          width={400}
+          height={300}
           src={galleryheader.src}
           alt={""}
           loading="eager"
@@ -56,40 +60,40 @@ export default function GallerySection() {
           className="!w-full"
           onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
         >
-          {GalleryImg.concat(GalleryImg).map(
-            (image: any, idx: number) => {
-              const isActive = idx === activeIndex;
-              const Opacity = isActive
-                ? "hover:!scale-[1.15] !transition-all"
-                : "brightness-50";
+          {GalleryImg.concat(GalleryImg).map((image: any, idx: number) => {
+            const isActive = idx === activeIndex;
+            const Opacity = isActive
+              ? "hover:!scale-[1.15] !transition-all"
+              : "brightness-50";
 
-              return (
-                <SwiperSlide
-                  key={idx}
-                  className={`pb-1 !flex !flex-col !justify-center !w-[200px] !h-[200px] md:!w-[300px] md:!h-[300px] lg:!w-[500px] lg:!h-[600px] ${Opacity}`}
+            return (
+              <SwiperSlide
+                key={idx}
+                className={`pb-1 !flex !flex-col !justify-center !w-[200px] !h-[200px] md:!w-[300px] md:!h-[300px] lg:!w-[500px] lg:!h-[600px] ${Opacity}`}
+              >
+                <div
+                  data-aos="fade-down"
+                  data-aos-duration="1000"
+                  data-aos-once="true"
+                  className={clsx(
+                    "relative bg-white  rounded-lg flex flex-col gap-2 justify-center",
+                    "lg:w-[500px] lg:h-[500px]",
+                    "md:w-[300px] md:h-[300px]",
+                    "w-[200px] h-[200px]"
+                  )}
                 >
-                  <div
-                    data-aos="fade-down"
-                    data-aos-duration="1000"
-                    data-aos-once="true"
-                    className={clsx(
-                      "relative bg-white  rounded-lg flex flex-col gap-2 justify-center",
-                      "lg:w-[500px] lg:h-[500px]","md:w-[300px] md:h-[300px]","w-[200px] h-[200px]"
-                    )}
-                  >
-                    <img
-                      width={500}
-                      height={500}
-                      alt=""
-                      src={image.src}
-                      loading="eager"
-                      className="object-cover w-full h-full rounded-lg"
-                    />
-                  </div>
-                </SwiperSlide>
-              );
-            }
-          )}
+                  <img
+                    width={500}
+                    height={500}
+                    alt=""
+                    src={image.src}
+                    loading="eager"
+                    className="object-cover w-full h-full rounded-lg"
+                  />
+                </div>
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
       </div>
     </section>
