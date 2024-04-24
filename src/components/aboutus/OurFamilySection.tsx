e11f43406d1d17397c5bce3acc931db7c951b25e
WNKing <77584564+BlazingKing@users.noreply.github.com>
2024-04-24 21:15:26 +0700

    style: postion new our family

--- src/components/aboutus/OurFamilySection.tsx
+++ src/components/aboutus/OurFamilySection.tsx
@@ -27,25 +27,43 @@ export default function OurFamilySection() {
           priority
         />
       </div>
-      <div className="p-2 w-full grid lg:grid-cols-3 lg:gap-5 l:grid-cols-1 l:gap-8 grid-cols-1 gap-5">
-        <div
-          onClick={() =>
-            window.open("https://www.baitongrestaurant.com/", "_blank")
-          }
-          data-aos="fade-left"
-          data-aos-once="true"
-          className="w-full h-full flex justify-center items-center cursor-pointer"
-        >
-          <Image
-            className="lg:w-[500px] lg:h-[180px] md:w-[420px] md:h-[220px] w-[200px] h-[120px]"
-            width={400}
-            height={400}
-            src={LogoImg1.src}
-            alt={""}
-            loading="eager"
-            priority
-          />
+      <div className="p-2 w-full flex flex-col gap-5">
+        <div className="w-full flex lg:flex-nowrap flex-wrap gap-2">
+          <div
+            onClick={() =>
+              window.open("https://www.baitongrestaurant.com/", "_blank")
+            }
+            data-aos="fade-left"
+            data-aos-once="true"
+            className="w-full h-full flex justify-center items-center cursor-pointer"
+          >
+            <Image
+              className="lg:w-[396px] lg:h-[218px] md:w-[396px] md:h-[218px] w-[250px] h-[150px]"
+              width={400}
+              height={400}
+              src={LogoImg1.src}
+              alt={""}
+              loading="eager"
+              priority
+            />
+          </div>
+          <div
+            data-aos="fade-right"
+            data-aos-once="true"
+            className="w-full h-full flex justify-center items-center cursor-pointer"
+          >
+            <Image
+              className="lg:w-[550px] lg:h-[218px] md:w-[550px] md:h-[218px] w-[280px] h-[110px]"
+              width={400}
+              height={400}
+              src={LogoImg3.src}
+              alt={""}
+              loading="eager"
+              priority
+            />
+          </div>
         </div>
+
         <div
           onClick={() =>
             window.open("https://www.noithaicuisine.com/", "_blank")
@@ -59,21 +77,6 @@ export default function OurFamilySection() {
             width={400}
             height={400}
             src={LogoImg2.src}
-            alt={""}
-            loading="eager"
-            priority
-          />
-        </div>
-        <div
-          data-aos="fade-right"
-          data-aos-once="true"
-          className="w-full h-full flex justify-center items-center cursor-pointer"
-        >
-          <Image
-            className="2xl:w-[600px] 2xl:h-[220px] xl:w-[500px] xl:h-[180px] lg:w-[500px] lg:h-[150px] md:w-[480px] md:h-[200px] w-[280px] h-[110px]"
-            width={400}
-            height={400}
-            src={LogoImg3.src}
             alt={""}
             loading="eager"
             priority
